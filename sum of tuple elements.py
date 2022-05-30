d0ce584fd756458160cfce8aaf81d563011cd4f0
vikashvarshney12 <106376654+vikashvarshney12@users.noreply.github.com>
2022-05-30 14:12:13 +0530

    Add files via upload

---
+++ sum of tuple elements.py
@@ -0,0 +1,11 @@
+
+test_tup = (7, 8, 9, 1, 10, 7) 
+  
+
+print("The original tuple is : " + str(test_tup)) 
+  
+
+res = sum(list(test_tup))
+  
+
+print("The summation of tuple elements are : " + str(res)) 
