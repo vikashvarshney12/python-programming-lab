0b458c25ff3793e485d12351931e3574123a5c25
vikashvarshney12 <106376654+vikashvarshney12@users.noreply.github.com>
2022-05-31 17:05:06 +0530

    Add files via upload

---
+++ input week number and print week name.py
@@ -0,0 +1,18 @@
+
+i=0
+while(i<1000):
+    a = int(input("enter the week no"))
+    if(a==1):
+        print("monday")
+    if(a==2):
+        print("tuesday")
+    if(a==3):
+        print("wednesday")
+    if(a==4):
+        print("thursday")
+    if(a==5):
+        print("friday")
+    if(a==6):
+        print("saturday")
+    if(a==7):
+        print("sunday")
